d5fafa8a041c8a5c23d4b2c1c4105f3f273cd185
Rameez Ahmed <noir.ramsam@gmail.com>
2021-07-29 15:57:20 +0500

    First design

---
+++ script.js
@@ -0,0 +1,129 @@
+
+//import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js'
+
+
+const canvas = document.querySelector('#bg')
+const scene = new THREE.Scene();
+const geometry = new THREE.TorusGeometry(.7, .2, 16, 100);
+const particlesGeometry = new THREE.BufferGeometry;
+const particleCnt = 10000;
+const loader = new THREE.TextureLoader()
+const redDot = loader.load('./assets/dotm.png')
+
+const posArray = new Float32Array(particleCnt * 3);
+for (let i = 0; i < particleCnt * 3; i++) {
+    posArray[i] = (Math.random() - 0.5) * 5
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(posArray, 3))
+
+
+const material = new THREE.PointsMaterial({
+    size: 0.01,
+    map: redDot,
+    transparent:true
+})
+
+const torusMaterial = new THREE.PointsMaterial({
+    size: 0.01,
+    color: 'lightgray'
+})
+//material.color = new THREE.Color('#880808')
+const sphere = new THREE.Points(geometry, torusMaterial)
+const particlesMesh = new THREE.Points(particlesGeometry, material)
+scene.add(particlesMesh)
+
+const pointLight = new THREE.PointLight(0xffffff, 0.1)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.Z = 4
+
+scene.add(pointLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 0
+camera.position.y = 0
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+// const controls = new OrbitControls(camera, canvas)
+// controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor(new THREE.Color('black'), 1)
+
+
+/**
+ * Animate
+ */
+
+const clock = new THREE.Clock()
+document.addEventListener('mousemove', animateParticles)
+
+let mouseX = 0;
+let mouseY = 0;
+
+function animateParticles(event) {
+    mouseY = event.clientY
+    mouseX = event.clientX
+}
+
+
+const tick = () => {
+
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    sphere.rotation.y = .5 * elapsedTime
+    particlesMesh.rotation.y = -.1 * elapsedTime
+    if (mouseX > 0) {
+        particlesMesh.rotation.x = -mouseY * (elapsedTime * 0.00008)
+        particlesMesh.rotation.y = -mouseX * (elapsedTime * 0.00008)
+    }
+
+    // Update Orbital Controls
+    // controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
